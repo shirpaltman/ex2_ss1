1a57ac241cf8fc6b4766e8488fb9594f34a09a69
shirpaltman <121901416+shirpaltman@users.noreply.github.com>
2024-02-06 14:08:23 +0200

    creating header file named my_mat.h

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "my_mat.h"
 
 int main() {
     char userChoice;
---
+++ my_mat.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef _NUMCLASS_H
+#define _NUMCLASS_H
+
+
+void scanMatrix();
+int ifHasPath(int i,int j);
+void floydWarshall(int matrix[10][10]);
+
+#endif
